c5c148f1c3de38f90334d3913ed0de6c2bb08568
Francisco Moreno <francisco.moreno.l@usach.cl>
2020-12-15 17:22:35 -0300

    Creada funcion verificador
    esta fincion verifica si existe alguna "reina" horizontal o verticalmente a la deseada("" en reina porque no son reinas jajajaja)

--- main.c
+++ main.c
@@ -59,18 +59,51 @@ void imprimirCiudad(int *ciudad,int col){
 	printf("\n");
     return;
 }
+// Entradas: arreglo ciudad, entero equivalente a la columna deseada a buscar y un entero del numero maximo de columnas
+// Salida: un entero llamado verificador, que si vale 1 no encontro fallas en la posicion buscada y si es un -1 ubo alguna que estuviera horizontal o diagonal a la buscada 
+int verificarCiudad(int *ciudad, int Ncolumna, int columnas){
+    int suma = Ncolumna + ciudad[Ncolumna]; // es la suma de las filas y columnas para verificar si tiene alguna diagonal
+    int resta = ciudad[Ncolumna]- Ncolumna;// lo mismo que el anterior pero con la resta
+    int contador = 0;
+    int verificador = 1;//verificador para saber si esta bien colocada o no, si es un 1 esta bien, si es un -1 mal
+    while(contador < columnas){//mientras el contador no pase por todas las columnas no se detendra, en si esto ira revisando si se encuentra otra red de forma horizontal o diagonal
+        
+        if(contador != Ncolumna){
+            if(ciudad[contador]!= -1){
+                if(ciudad[contador] == ciudad[Ncolumna]){
+                    verificador = -1;
+                    contador = columnas +1;
+                }
+                else if (ciudad[contador] + contador == suma){
+                    verificador = -1;
+                    contador = columnas +1;
+                }
+                else if (ciudad[contador] - contador == resta){
+                    verificador = -1;
+                    contador = columnas +1;
+                }
+                
+
+            }
+        }
+         contador++;
+    }
+    return verificador;
+}
 
 int main(int argc, char const *argv[])
 {
     //Hola mundo
     int columnas,filas;
     int *ciudad = openFile(argv[1],&columnas,&filas);
+   // ciudad[4] = 7; esto solo esta porque anduve probando si funcionaba
     imprimirCiudad(ciudad,columnas);
-
+    int verificador = verificarCiudad(ciudad,5,columnas);
+   // printf("el verificador vale: %d \n",verificador);
     free(ciudad);
 
 
-//hola como estas
+
 
     return 0;
 }
